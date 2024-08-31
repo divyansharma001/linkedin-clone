586daa3862745a05d9d28ab485d27ef43601acd4
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-08-31 16:48:41 +0530

    Complete Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import Profilephoto from "./shared/Profilephoto";
 
 const Sidebar = ({ user }: { user: any }) => {
-  console.log(user?.externalAccounts[0]?.username);
+  
   return (
-    <div className="hidden md:block w-[20%] h-fit border-gray-300 bg-white rounded">
+    <div className="hidden md:block w-[20%] h-fit border border-gray-30000 bg-white rounded">
       <div className="flex relative flex-col items-center">
         <div className="w-full h-16 overflow-hidden">
           {user && (
@@ -20,27 +20,34 @@ const Sidebar = ({ user }: { user: any }) => {
         </div>
 
         <div className="my-1 absolute top-10">
-        <Profilephoto src={user ? user?.imageUrl : '/user.jpg'} />
+          <Profilephoto src={user ? user?.imageUrl : "/user.jpg"} />
         </div>
 
         <div className="border-b border-b-gray-300">
           <div className="p-2 mt-5 text-center">
-
-            <h1 className="font-semibold hover:underline cursor-pointer">{user ? `${user?.firstName} ${user?.lastName}` : "Login to continue"}</h1>
-<p className="text-xs">@{user ? `${user?.externalAccounts[0]?.username}` : "username"}</p>
+            <h1 className="font-semibold hover:underline cursor-pointer">
+              {user
+                ? `${user?.firstName} ${user?.lastName}`
+                : "Login to continue"}
+            </h1>
+            <p className="text-xs">
+              @{user ? `${user?.externalAccounts[0]?.username}` : "username"}
+            </p>
           </div>
- 
-          <div className="text-xs">
-            <div className="w-full flex justify-between items-center py-2 hover:bg-gray-200">
-
-            </div>
-             
-          </div>
-
         </div>
-
-        <div className="my-1 absolute top-10"></div>
       </div>
+      <div className="text-xs m-2">
+        <div className="w-full flex justify-between items-center py-2 hover:bg-gray-200">
+          <p>Post Impression</p>
+          <p className="text-blue-600">99</p>
+        </div>
+        <div className="w-full flex justify-between items-center py-2 hover:bg-gray-200">
+          <p>Posts</p>
+          <p className="text-blue-600">0</p>
+        </div>
+      </div>
+
+      <div className="my-1 absolute top-10"></div>
     </div>
   );
 };
