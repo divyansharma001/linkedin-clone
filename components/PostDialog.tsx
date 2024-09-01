0aa32844941a5b9584e108b315b00dfbd41c0ede
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-09-01 12:15:23 +0530

    chore: Add image upload functionality to PostDialog component

--- components/PostDialog.tsx
+++ components/PostDialog.tsx
@@ -11,6 +11,10 @@ import {
 import { Input } from "@/components/ui/input";
 import Profilephoto from "./shared/Profilephoto";
 import { Textarea } from "./ui/textarea";
+import Image from "next/image";
+import { ImageIcon } from "lucide-react";
+import { useRef, useState } from "react";
+import { readFileAsDataUrl } from "@/lib/utils";
 
 export function Postdialog({
   setOpen,
@@ -21,6 +25,18 @@ export function Postdialog({
   open: boolean;
   src: string;
 }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [selectedFile, setSelectedFile] = useState<string>("");
+  const fileChangeHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const event = e.target;
+    if (event && event.files && event.files.length > 0) {
+      const file = event.files[0];
+      if (file) {
+        const dataUrl = await readFileAsDataUrl(file);
+        setSelectedFile(dataUrl);
+      }
+    }
+  };
   return (
     <Dialog open={open}>
       <DialogContent
@@ -35,20 +51,48 @@ export function Postdialog({
               <p className="text-xs text-gray-500 pt-1">Post to anyone</p>
             </div>
           </DialogTitle>
-         
         </DialogHeader>
         <form action="">
           <div className="flex flex-col">
-            <Textarea 
-            id="name"
-            name="inputText"
-            className="border-none text-lg focus-visible:ring min-h-[150px]"
-            placeholder="What do you want to talk about?" />
+            <Textarea
+              id="name"
+              name="inputText"
+              className="border-none text-lg min-h-[150px]"
+              placeholder="What do you want to talk about?"
+            />
+            <div className="my-4">
+              {selectedFile && (
+                <div className="flex justify-center mt-4">
+                  <Image
+                    src={selectedFile}
+                    width={800}
+                    height={800}
+                    objectFit="contain"
+                    alt="full-image"
+                  />
+                </div>
+              )}
+            </div>
           </div>
+
+          <DialogFooter>
+            <div className="flex items-center gap-4">
+              <input
+                type="file"
+                name="image"
+                ref={inputRef}
+                onChange={fileChangeHandler}
+                className="hidden"
+                accept="image/*"
+              />
+              <Button type="submit">Post</Button>
+            </div>
+          </DialogFooter>
         </form>
-        <DialogFooter>
-          <Button type="submit">Post</Button>
-        </DialogFooter>
+        <Button variant={"ghost"} onClick={() => inputRef?.current?.click()}>
+          <ImageIcon className="text-blue-500" />
+          <p>Media</p>
+        </Button>
       </DialogContent>
     </Dialog>
   );
