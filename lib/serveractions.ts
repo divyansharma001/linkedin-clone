9d08cec7d09cd21140327f0c4ee68145e7e20e54
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-09-05 00:17:36 +0530

    chore: Update cloudinary configuration for image uploads

--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -6,7 +6,14 @@ import {PrismaClient} from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-import { v2 as cloudinary } from 'cloudinary';
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
